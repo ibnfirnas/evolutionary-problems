65338472719d6115926c5c19ba1fdfccafb3d1ae
Siraaj Khandkar <siraaj@khandkar.net>
2012-09-20 17:44:51 -0400

    Oops, month sequence is actually 0-indexed.

--- 8-queens/src/eight_queens.ml
+++ 8-queens/src/eight_queens.ml
@@ -37,7 +37,7 @@ let time_stamp =
   let tm = time () |> localtime in
   sprintf
   "%04d-%02d-%02d--%02d-%02d-%02d"
-  (tm.tm_year + 1900) tm.tm_mon tm.tm_mday tm.tm_hour tm.tm_min tm.tm_sec
+  (tm.tm_year + 1900) (tm.tm_mon + 1) tm.tm_mday tm.tm_hour tm.tm_min tm.tm_sec
 
 
 let path_of components =
